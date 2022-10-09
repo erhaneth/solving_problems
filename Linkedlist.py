f09d1921c5b1ee062c4a621a99e6c4efe3845055
“erhaneth” <gumusucb21@gmail.com>
2022-10-08 20:22:47 -0700

    Practice

---
+++ Linkedlist.py
@@ -0,0 +1,55 @@
+# create classes, blueprint for nodes:
+class Node:
+    # constructor
+    # first parameter always it"self"
+    def __init__(self, data, next=None):
+        self.data = data
+        # def __init__(self, data, taco)
+        # self.next = taco
+        self.next = next
+    def __str__(self):
+        return self.data
+  
+# A new class that use Node class
+class Linked_List:
+    def __init__(self):
+        self.head=None
+        self.tail=None 
+        self.size=0
+    def __str__(self):
+        if self.size == 0:
+            return '[]'
+        else:
+            current = self.head
+            result =str(current)
+            while current.next:
+                result+= f',{str(current.next)}'
+                current=current.next
+            return f"[{result}]"
+    # method inside of class list
+    def insert_front(self, data):
+        #  the list is empty
+        if(self.size==0):
+        # the list contains 1 node already
+         self.head = Node(data)
+         self.tail = self.head
+        elif(self.size == 1):
+        # there are 2 or more nodes already in the list
+         self.head = Node(data,self.tail)
+        else:
+         self.head = Node(data, self.head)  
+        self.size += 1
+
+# created a new node
+
+my_list = Linked_List()
+my_list.insert_front("Heg")
+my_list.insert_front("Gumus")
+my_list.insert_front("Amed")
+my_list.insert_front("Block")
+my_list.insert_front("erika")
+
+print(my_list)
+
+
+        
