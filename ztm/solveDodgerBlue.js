5e183f5b42b3aa56e889ed13162e51eceda1187e
“erhaneth” <gumusucb21@gmail.com>
2022-10-13 21:50:18 -0700

    Found the nemo ineffincient way

--- ztm/solveDodgerBlue.js
+++ ztm/solveDodgerBlue.js
@@ -1,11 +1,16 @@
 const nemo = ['nemo'];
+const everychar = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
 
+const large = new Array(1000).fill('nemo');
 function findNemo(array) {
+    let time0 = performance.now();
     for (let i =0; i < array.length; i++ ){
         if(array[i]==='nemo'){
             console.log("Found NEMO!");
         }
     }
+    let time1 = performance.now();
+    console.log('Call to find Nemo took ' + (time1-time0) + " milliseconds");
 }
-findNemo(nemo)
-console.log("helooooo");
+findNemo(large)
+// console.log("helooooo");
