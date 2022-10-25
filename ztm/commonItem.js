6738fa011856b701bf45a3c1875c7859ecce29ce
“erhaneth” <gumusucb21@gmail.com>
2022-10-25 13:33:05 -0700

    Solved diff version

--- ztm/commonItem.js
+++ ztm/commonItem.js
@@ -14,9 +14,9 @@ const array2 = ["z", "y", 3];
 //create a function named commonItem
 function commonItem(array1, array2) {
   // for loop
-  for (let i = 0; i < array1.length; i++) {
+  for (let i = 0; i < array1.length; i++) {  //O(n)
     //2nd for loop
-    for (let j = 0; j < array2.length; j++)
+    for (let j = 0; j < array2.length; j++) // O(n)
       if (array1[i] === array2[j]) {
         return true;
       }
@@ -26,3 +26,39 @@ function commonItem(array1, array2) {
 }
 //invoke it, to see what would return
 console.log("Here the result", commonItem(array1, array2))
+
+//Big O(n*n) not very efficient 
+
+// More efficient way
+const arr1 = ['a', 'b', 'c', 'x'];
+const arr2 = ['z', 'y', '4'];
+
+function containCommonItem(arr1, arr2) {
+    //loop tru first array and create object where properties === items in the array
+    let match = {};
+    for (let i = 0; i < arr1.length; i++){
+        if(!match[arr1[i]]){
+            const item = arr1[i]
+            match[item] = true;
+        }
+
+    }
+    // loop tru second array and check if item in second array exists on created object
+    for ( let j = 0; j < arr2.length; j++) {
+        if(match[arr2[j]]){
+            return true;
+        }
+    }
+    return false;
+
+}
+// O(a+b) Time complexity 
+// O(a) Space complexity 
+console.log("Efficient way", containCommonItem(arr1,arr2))
+
+//JS method lol
+function containsCommonItem3(arr1, arr2) {
+    return arr1.some(item => arr2.includes(item))
+  }
+
+  console.log("Sneaky way", containsCommonItem3(array1, array2))
