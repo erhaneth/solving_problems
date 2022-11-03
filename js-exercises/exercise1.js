92ec42df82d19838d5e3e568b547481fca9cc32a
“erhaneth” <gumusucb21@gmail.com>
2022-11-02 23:29:02 -0700

    Class objects

---
+++ js-exercises/exercise1.js
@@ -0,0 +1,36 @@
+var object1 = { value: 20 };
+var object2 = object1;
+var object3 = { value: 20}
+console.log(object3)
+
+// context vs scope 
+
+const object4 = {
+    a: function(){
+        console.log(this)
+    }
+}
+
+// instantiation 
+class Player {
+    constructor(name,type){
+        console.log('player', this)
+        this.name = name;
+        this.type = type;
+    }
+    introduce() {
+        console.log(`Hi I am${this.name}, I'm a ${this.type}`)
+    }
+}
+class Wizard extends Player {
+    constructor(name,type){
+        super(name, type)
+        console.log('wizard', this);
+    }      
+    play(){
+        console.log(`Weyy I'm ${this.type}`)
+    }
+}
+
+const wizard1 = new Wizard('Grenny', 'Healer')
+const wizard2 = new Wizard('Darky', 'Killer')
