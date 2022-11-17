6070708e205953f13dd94bef36c58acb9687bc18
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2022-11-17 12:38:14 -0800

    test

--- datastructure/arrays/test.js
+++ datastructure/arrays/test.js
@@ -1,18 +1,1 @@
-import { Navbar, Transactions, Welcome, Footer } from "./components";
-
-function App() {
-  return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
-      </div>
-      <Transactions />
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
-
-//sjdufiusaf
+//This is just test file
