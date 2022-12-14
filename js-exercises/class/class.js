56e56df06e715a132e74efd1cae07430f4f3458c
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2022-12-13 22:48:12 -0800

    shape the object

---
+++ js-exercises/class/class.js
@@ -0,0 +1,12 @@
+// https://university.alchemy.com/course/js/sc/5d7698b6e5a95ac05652f732/stage/5d7698ffe5a95ac05652f733
+// JavaScript does not have classes in a traditional sense. It has prototypes. 
+
+// Our Shape "Constructor"
+function Shape(x, y) {
+    // store x and y in this.position
+   this.position = {
+    x: x,
+    y: y
+  }
+
+}
